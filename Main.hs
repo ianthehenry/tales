9613cb2421d138e1e1aa50f8f5d682d24328b623
Ian Henry <ianthehenry@gmail.com>
2014-07-13 00:06:32 -0400

    implemented all commands with an MVar. up next: persistence

--- Main.hs
+++ Main.hs
@@ -1,46 +1,95 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
-import Data.Text (Text, breakOn, stripStart)
+import Data.Text (Text, breakOn, stripStart, intercalate, pack, unpack, length)
+import Text.Read (readMaybe)
 import Data.Text.Lazy (fromStrict)
 import Web.Scotty
 import Network.Wai.Middleware.RequestLogger
 import Control.Lens
+import Control.Monad.IO.Class (liftIO)
 import Data.Monoid (mconcat)
-import Data.Functor ((<$>))
+import qualified Control.Concurrent.MVar as MVar
 
 type User = String
-data LogEntry = LogEntry {story :: Text, user :: User} deriving (Show, Eq)
-type BookOfTales = [LogEntry]
+data LogEntry = LogEntry User Text deriving (Show, Eq)
+data BookOfTales = BookOfTales User [LogEntry]
 
-currentUser :: BookOfTales -> User
-currentUser (x:_) = user x
-
--- BEHAVE IN THIE FOLLOWING MANNER:
--- /tales
---   read [page] (if page is omitted, it returns the index. page is just the index of the entry)
---   write username Text...
-
-token = "MEv3EuIShbsE73cIMRd3C9d1"
+holder :: BookOfTales -> User
+holder (BookOfTales h _) = h
 
 firstWord :: Text -> (Text, Text)
 firstWord input = breakOn " " input & _2 %~ stripStart
 
-dispatch :: (Text, Text) -> Text
-dispatch ("", _) = "Welcome to the book of tales! Possible commands are:\nread - list pages filled by tales past\nread page - read a tale\nwrite username tale - write a tale, and hand the book to another wanderer"
-dispatch ("help", _) = dispatch ("", "")
-dispatch ("read", "") = "no entries yet"
-dispatch ("read", _) = "reading a specific page ..."
-dispatch ("write", _) = "writing"
-dispatch ("steal", _) = "you have failed to steal, and have lost 1 xp"
-dispatch (cmd, _) = mconcat ["unrecognized command ", cmd, "!"]
+helperText :: Text
+helperText = "Observed commands are:\nread - list pages filled by tales past\nread page - read a tale\nwrite username tale - write a tale, and hand the book to another wanderer"
 
-tales :: Text -> Text
-tales = dispatch . firstWord
+pshow :: Show a => a -> Text
+pshow = pack . show
+
+prettyPrintIndex :: Int -> LogEntry -> Text
+prettyPrintIndex page (LogEntry user story) = mconcat [pshow page, ". ", pack user, " (", pshow (Data.Text.length story), " characters)"]
+
+prettyPrintStory :: LogEntry -> Text
+prettyPrintStory (LogEntry _ story) = story
+
+downFrom :: Int -> [Int]
+downFrom x = [x, pred x..]
+
+bookIndex :: BookOfTales -> Text
+bookIndex (BookOfTales _ entries) = intercalate "\n" $ "All tales written in an age forgotten:" :
+  zipWith prettyPrintIndex (downFrom pageCount) entries
+  where pageCount = Prelude.length entries
+
+readBook :: BookOfTales -> Text -> Text
+readBook book "" = bookIndex book
+readBook (BookOfTales _ entries) input = case readMaybe (unpack input) of
+  Nothing -> "I don't understand that page number :/"
+  Just n | n > 0 && n <= pageCount -> prettyPrintStory (entries !! (pageCount - n))
+         | otherwise               -> "Invalid page"
+  where pageCount = Prelude.length entries
+
+writeBook :: BookOfTales -> User -> Text -> (BookOfTales, Text)
+writeBook oldBook@(BookOfTales oldUser entries) newUser story
+  | Data.Text.length story == 0 = (oldBook, mconcat ["The price of holding the Book of Tales is that ",
+                                                     "you must write in its pages -- a hard responsibility, ",
+                                                     "I know, but one that ", pack oldUser, " has placed upon your shoulders."])
+  | oldUser == newUser = (oldBook, "I appreciate your enthusiasm, but the Book of Tales must change hands in order for the ink to take. It's part of... the magic.")
+  | otherwise = let newBook  = BookOfTales newUser $ LogEntry oldUser story : entries
+                    response = mconcat ["Your tale passes into legend; the Book passes safely to ", pack newUser, "'s hands."] in
+                (newBook, response)
+
+extractUser :: Text -> (User, Text)
+extractUser input = firstWord input & _1 %~ unpack
+
+dispatch :: BookOfTales -> (Text, Text) -> (BookOfTales, Text)
+dispatch book ("", _)          = (book, mconcat ["Welcome to the book of tales! ", helperText])
+dispatch book ("help", _)      = (book, helperText)
+dispatch book ("read", input)  = (book, readBook book input)
+dispatch book ("write", input) = (uncurry $ writeBook book) (extractUser input)
+dispatch book ("steal", _)     = (book, "No; I took stealing out because it was unnecessarily complicated.")
+dispatch book (cmd, _)         = (book, mconcat ["Unrecognized command \"", cmd, "\". ", helperText])
+
+tales :: BookOfTales -> User -> Text -> (BookOfTales, Text)
+tales book requestor input
+  | requestor == holder book = dispatch book $ firstWord input
+  | otherwise                = (book, "You do not hold the Book of Tales!")
+
+incomingMessage :: MVar.MVar BookOfTales -> ActionM ()
+incomingMessage bookVar = do
+  user <- param "user"
+  input <- param "text"
+  book <- liftIO $ MVar.readMVar bookVar
+  let (newBook, response) = tales book user input
+  liftIO $ MVar.swapMVar bookVar newBook
+  text . fromStrict $ response
 
 main :: IO ()
-main = scotty 3000 $ do
-  middleware logStdoutDev
-  post "/" $ (text . fromStrict) =<<
-    tales <$> (param "text")
+main = do
+  let book = BookOfTales "ian" [LogEntry "current" "Another story that someone added", LogEntry "ian" "The book of tales is forged in the fires of Mount Haskell"]
+  db <- MVar.newMVar book
+  scotty 3000 $ do
+    middleware logStdoutDev
+    post "/" (incomingMessage db)
+
 
