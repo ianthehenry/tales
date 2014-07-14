83cf8e7d3e76baaeff623288419629751e54886c
Ian Henry <ianthehenry@gmail.com>
2014-07-13 23:38:16 -0400

    added persistence of tales (though not the holder)

--- Main.hs
+++ Main.hs
@@ -9,11 +9,18 @@ import Network.Wai.Middleware.RequestLogger
 import Control.Lens
 import Control.Monad.IO.Class (liftIO)
 import Data.Monoid (mconcat)
+import Database.HDBC.Sqlite3 (connectSqlite3, Connection)
 import Control.Concurrent.MVar (MVar, modifyMVar, newMVar)
+import Database.HDBC (prepare, execute, commit, fetchAllRows)
+import Database.HDBC.SqlValue (SqlValue, fromSql, toSql)
+import Data.Time (UTCTime, getCurrentTime)
+import Data.Time.Format (formatTime)
+import System.Locale (defaultTimeLocale)
 
 type User = String
-data LogEntry = LogEntry User Text deriving (Show, Eq)
-data BookOfTales = BookOfTales User [LogEntry]
+data LogEntry = LogEntry User Text UTCTime deriving (Show, Eq)
+data BookOfTales = BookOfTales User [LogEntry] deriving (Show, Eq)
+data NewBook = SameBook | NewEntry User Text
 
 holder :: BookOfTales -> User
 holder (BookOfTales h _) = h
@@ -22,16 +29,20 @@ firstWord :: Text -> (Text, Text)
 firstWord input = breakOn " " input & _2 %~ stripStart
 
 helperText :: Text
-helperText = "Observed commands are:\nread - list pages filled by tales past\nread page - read a tale\nwrite username tale - write a tale, and hand the book to another wanderer"
+helperText = "Valid commands:\nread - list pages filled by tales past\nread page - read a tale\nwrite username tale - write a tale, and hand the book to another wanderer"
 
 pshow :: Show a => a -> Text
 pshow = pack . show
 
 prettyPrintIndex :: Int -> LogEntry -> Text
-prettyPrintIndex page (LogEntry user story) = mconcat [pshow page, ". ", pack user, " (", pshow (Data.Text.length story), " characters)"]
+prettyPrintIndex page (LogEntry user story _) = mconcat [pshow page, ". ", pack user, " (", pshow (Data.Text.length story), " characters)"]
+
+prettyPrintTime :: UTCTime -> Text
+prettyPrintTime t = pack $ formatTime defaultTimeLocale "%b %e" t
 
 prettyPrintStory :: LogEntry -> Text
-prettyPrintStory (LogEntry _ story) = story
+prettyPrintStory (LogEntry username story timestamp) =
+  mconcat ["By ", pack username, " on ", prettyPrintTime timestamp, "\n\n", story]
 
 downFrom :: Int -> [Int]
 downFrom x = [x, pred x..]
@@ -50,43 +61,77 @@ readBook (BookOfTales _ entries) input = case readMaybe (unpack input) of
          | otherwise               -> "Invalid page"
   where pageCount = Prelude.length entries
 
-writeBook :: BookOfTales -> User -> Text -> (BookOfTales, Text)
-writeBook oldBook@(BookOfTales oldUser entries) newUser story
-  | Data.Text.length story == 0 = (oldBook, mconcat ["The price of holding the Book of Tales is that ",
-                                                     "you must write in its pages -- a hard responsibility, ",
-                                                     "I know, but one that ", pack oldUser, " has placed upon your shoulders."])
-  | oldUser == newUser = (oldBook, "I appreciate your enthusiasm, but the Book of Tales must change hands in order for the ink to take. It's part of... the magic.")
-  | otherwise = let newBook  = BookOfTales newUser $ LogEntry oldUser story : entries
-                    response = mconcat ["Your tale passes into legend; the Book passes safely to ", pack newUser, "'s hands."] in
-                (newBook, response)
+writeBook :: BookOfTales -> User -> Text -> (NewBook, Text)
+writeBook (BookOfTales oldUser _) newUser story
+  | Data.Text.length story == 0 = (SameBook, mconcat ["The price of holding the Book of Tales is that ",
+                                                      "you must write in its pages -- a hard responsibility, ",
+                                                      "I know, but one that ", pack oldUser, " has placed upon your shoulders."])
+  | oldUser == newUser          = (SameBook, "I appreciate your enthusiasm, but the Book of Tales must change hands in order for the ink to take. It's part of... the magic.")
+  | otherwise                   = (NewEntry newUser story, mconcat ["Your tale passes into legend; the Book passes safely to ", pack newUser, "'s hands."])
 
 extractUser :: Text -> (User, Text)
 extractUser input = firstWord input & _1 %~ unpack
 
-dispatch :: BookOfTales -> (Text, Text) -> (BookOfTales, Text)
-dispatch book ("", _)          = (book, mconcat ["Welcome to the book of tales! ", helperText])
-dispatch book ("help", _)      = (book, helperText)
-dispatch book ("read", input)  = (book, readBook book input)
+dispatch :: BookOfTales -> (Text, Text) -> (NewBook, Text)
+dispatch _ ("", _)             = (SameBook, mconcat ["Welcome to the book of tales! ", helperText])
+dispatch _ ("help", _)         = (SameBook, helperText)
+dispatch book ("read", input)  = (SameBook, readBook book input)
 dispatch book ("write", input) = (uncurry $ writeBook book) (extractUser input)
-dispatch book ("steal", _)     = (book, "No; I took stealing out because it was unnecessarily complicated.")
-dispatch book (cmd, _)         = (book, mconcat ["Unrecognized command \"", cmd, "\". ", helperText])
+dispatch _ ("steal", _)        = (SameBook, "No; I took stealing out because it was unnecessarily complicated.")
+dispatch _ (cmd, _)            = (SameBook, mconcat ["Unrecognized command \"", cmd, "\". ", helperText])
 
-tales :: User -> Text -> BookOfTales -> (BookOfTales, Text)
+tales :: User -> Text -> BookOfTales -> (NewBook, Text)
 tales requestor input book
   | requestor == holder book = dispatch book $ firstWord input
-  | otherwise                = (book, "You do not hold the Book of Tales!")
+  | otherwise                = (SameBook, "You do not hold the Book of Tales!")
 
-incomingMessage :: MVar BookOfTales -> ActionM ()
-incomingMessage bookVar = do
+entryToSql :: LogEntry -> [SqlValue]
+entryToSql (LogEntry username story timestamp) =
+  [toSql username, toSql story, toSql timestamp]
+
+insertLogEntry :: Connection -> LogEntry -> IO ()
+insertLogEntry c newEntry = do
+    insert <- prepare c "INSERT INTO entries VALUES (?, ?, ?);"
+    execute insert (entryToSql newEntry)
+    commit c
+
+notifyNewHolder :: User -> IO ()
+notifyNewHolder user =
+  print $ mconcat ["Passing to ", user]
+
+incomingMessage :: MVar BookOfTales -> Connection -> ActionM ()
+incomingMessage bookVar conn = do
   user <- param "user_name"
   input <- param "text"
-  response <- liftIO $ modifyMVar bookVar (return . tales user input)
+
+  response <- liftIO $ modifyMVar bookVar (\book ->
+    case tales user input book of
+      (SameBook, response) -> return (book, response)
+      (NewEntry newHolder story, response) -> do
+        time <- liftIO getCurrentTime
+        let newEntry = LogEntry oldHolder story time
+        liftIO $ insertLogEntry conn newEntry
+        liftIO $ notifyNewHolder newHolder
+        return (BookOfTales newHolder (newEntry:oldEntries), response)
+        where (BookOfTales oldHolder oldEntries) = book)
+
   text . fromStrict $ response
+
+entryFromSql :: [SqlValue] -> LogEntry
+entryFromSql [username, story, timestamp] = LogEntry (fromSql username) (fromSql story) (fromSql timestamp)
+
+initialBook :: Connection -> IO BookOfTales
+initialBook c = do
+  select <- prepare c "SELECT username, story, timestamp FROM entries ORDER BY timestamp DESC;"
+  execute select []
+  result <- fetchAllRows select
+  return $ BookOfTales "ian" (map entryFromSql result)
 
 main :: IO ()
 main = do
-  let book = BookOfTales "ian" [LogEntry "anonymous" "The Book of Tales is forged in the fires of Mount Haskell"]
-  db <- newMVar book
+  conn <- connectSqlite3 "book.db"
+  book <- initialBook conn
+  var <- newMVar book
   scotty 3000 $ do
     middleware logStdoutDev
-    post "/" (incomingMessage db)
+    post "/" (incomingMessage var conn)
